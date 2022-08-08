62a63944be6fa16dc2b565c3a306c9949ffad8a5
Alexander Smirnov <alexander.smirnov@lge.com>
2022-08-08 20:57:30 +0300

    [Engine] Update master lua file

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "ImGui"
     kind "StaticLib"
     language "C++"
+    staticruntime "off"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -12,11 +13,12 @@ project "ImGui"
         "imgui.cpp",
         "imgui_draw.cpp",
         "imgui_internal.h",
+        "imgui_tables.cpp",
         "imgui_widgets.cpp",
         "imstb_rectpack.h",
         "imstb_textedit.h",
         "imstb_truetype.h",
-	"imgui_demo.cpp"
+	    "imgui_demo.cpp"
     }
 
 	filter "system:windows"
